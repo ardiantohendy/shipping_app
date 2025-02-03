204263e8ad58e351b6665149cda869c84bb6d265
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-03 15:19:58 +0700

    new commit (-> CRUD for Cargo datas)

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Cargo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    jenis_barang = models.CharField(max_length=100)
+    berat = models.FloatField()
+    ukuran = models.FloatField()
+    agen_pengiriman = models.CharField(max_length=100)
+    tujuan_pengiriman = models.CharField(max_length=100)
+    nama_pengirim = models.CharField(max_length=100)
+    nama_penerima = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.jenis_barang} - {self.nama_penerima}'
